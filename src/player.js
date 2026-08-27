512267c57ffa9b0c35daf83c122f3f147897b621
eomjinseong-art <eomjinseong@gmail.com>
2026-08-27 12:02:02 +0900

    Initial commit: English Learning App MVP

---
+++ src/player.js
@@ -0,0 +1,32 @@
+// YouTube Player Module
+export class YoutubePlayer {
+  constructor(containerId) {
+    this.container = document.getElementById(containerId);
+    this.player = null;
+    this.currentTime = 0;
+  }
+
+  loadVideo(videoId) {
+    // YouTube iframe 생성
+    const iframe = document.createElement('iframe');
+    iframe.width = '100%';
+    iframe.height = '400';
+    iframe.src = `https://www.youtube.com/embed/${videoId}`;
+    iframe.frameborder = '0';
+    iframe.allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture';
+    iframe.allowFullscreen = true;
+
+    this.container.innerHTML = '';
+    this.container.appendChild(iframe);
+  }
+
+  getCurrentTime() {
+    // 나중에 YouTube API로 구현
+    return this.currentTime;
+  }
+
+  seekTo(seconds) {
+    this.currentTime = seconds;
+    // 나중에 YouTube API로 구현
+  }
+}
